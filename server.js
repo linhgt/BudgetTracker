db68ae248e1cbff623b7eb3f4be823e30b433acd
Linh Nguyen <linhnhatmen@gmail.com>
2021-02-12 23:11:39 +0700

    small change

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
